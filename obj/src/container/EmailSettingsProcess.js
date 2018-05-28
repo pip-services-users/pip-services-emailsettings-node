7fe4bca750f7d5ebfc5337f9835302f989501c4f
pipdeveloper <pipdevs@gmail.com>
2018-05-28 18:04:14 +0000

    project build by Travis CI [skip ci]

--- obj/src/container/EmailSettingsProcess.js
+++ obj/src/container/EmailSettingsProcess.js
@@ -5,6 +5,7 @@ const pip_services_net_node_1 = require("pip-services-net-node");
 const pip_services_oss_node_1 = require("pip-services-oss-node");
 const pip_clients_activities_node_1 = require("pip-clients-activities-node");
 const pip_clients_msgtemplates_node_1 = require("pip-clients-msgtemplates-node");
+const pip_clients_email_node_1 = require("pip-clients-email-node");
 const EmailSettingsServiceFactory_1 = require("../build/EmailSettingsServiceFactory");
 class EmailSettingsProcess extends pip_services_container_node_1.ProcessContainer {
     constructor() {
@@ -12,6 +13,7 @@ class EmailSettingsProcess extends pip_services_container_node_1.ProcessContaine
         this._factories.add(new EmailSettingsServiceFactory_1.EmailSettingsServiceFactory);
         this._factories.add(new pip_clients_activities_node_1.ActivitiesClientFactory());
         this._factories.add(new pip_clients_msgtemplates_node_1.MessageTemplatesClientFactory());
+        this._factories.add(new pip_clients_email_node_1.EmailClientFactory());
         this._factories.add(new pip_services_net_node_1.DefaultNetFactory);
         this._factories.add(new pip_services_oss_node_1.DefaultOssFactory);
     }
