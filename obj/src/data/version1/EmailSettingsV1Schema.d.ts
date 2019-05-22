a8018f2f8697ea533875a7d84031ec73c9c454a1
pipdeveloper <pipdevs@gmail.com>
2019-05-22 07:44:46 +0000

    project build by Travis CI [skip ci]

--- obj/src/data/version1/EmailSettingsV1Schema.d.ts
+++
@@ -1,4 +0,0 @@
-import { ObjectSchema } from 'pip-services3-commons-node';
-export declare class EmailSettingsV1Schema extends ObjectSchema {
-    constructor();
-}
--- obj/src/persistence/EmailSettingsMongoDbSchema.d.ts
+++
@@ -1,2 +0,0 @@
-import { Schema } from 'mongoose';
-export declare let EmailSettingsMongoDbSchema: (collection?: string) => Schema;
